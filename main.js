4294fbe38fe9f4055fb6e930e30f3842e7eb6766
James Brattin <james.brattin@gmail.com>
2024-02-18 19:38:08 +0100

    remove console logs

--- main.js
+++ main.js
@@ -8,17 +8,14 @@ if (window.Worker) {
 
     first.onchange = function () {
         myWorker.postMessage([first.value, second.value]);
-        console.log('Message posted to worker');
     }
 
     second.onchange = function () {
         myWorker.postMessage([first.value, second.value]);
-        console.log('Message posted to worker');
     }
 
     myWorker.onmessage = function (e) {
         result.textContent = e.data;
-        console.log('Message received from worker');
     }
 } else {
     console.log('Your browser doesn\'t support web workers.');
